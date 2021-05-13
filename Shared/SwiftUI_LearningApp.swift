337cb9ae7de12253d862db8db7619faced039d05
“Sam” <gztzxzp@163.com>
2021-05-13 16:31:33 +0800

    Initial Commit

---
+++ Shared/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  Shared
+//
+//  Created by Sam on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
